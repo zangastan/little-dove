f1ef2242a0c5e138464e10b4fca3a9ef73b3e504
Augustine Kasolota <kasolotaaugustine@gmail.com>
2026-01-16 23:04:19 +0200

    second commit

---
+++ src/components/fooCards.vue
@@ -0,0 +1,6 @@
+<template>
+        <div class="shadow-lg rounded-md p-3 w-md m-auto">
+            <h2 class="font-bold">Food item</h2>
+            <p>This is the description of the food item stated above</p>
+    </div>
+</template>
